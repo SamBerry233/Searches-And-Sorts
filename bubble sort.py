f70a4b1027f771cd02453f12e1fcc56bbe859f90
Sam Berry <bsam.1479x@gmail.com>
2025-11-24 10:29:43 +0000

    bubble sort
    Added comments to explain the bubble sort algorithm.

--- bubble sort.py
+++ bubble sort.py
@@ -1,17 +1,18 @@
 items = [23, 41, 11, 17, 34, 56]
-n = len(items)
-Swaps = 1
+n = len(items) # defines the number of items in the list
+Swaps = 1 # allows the while loop to run
 Passes = 0
-while Swaps != 0:
-    Swaps = 0
-    First = 0
-    Last = First + 1
+while Swaps != 0: # if the last pass made any difference, it could be useful to do it again
+    Swaps = 0 # the current pass has not done anything yet
+    First = 0 # the first index in the list
+    Last = First + 1 #the second value to be compared
     Passes += 1
-    while Last < n:
-        if items[First] > items[Last]:
-            items[First], items[Last] = items[Last], items[First]
-            Swaps += 1
-        First += 1
+    while Last < n: # if every item was compared
+        if items[First] > items[Last]: # if the second item is smaller
+            items[First], items[Last] = items[Last], items[First] # move the first item forward
+            Swaps += 1 # the items are now swapped
+        First += 1 # move onto the next item
         Last = First + 1
 
-print(items)
+print(items) # output
+
