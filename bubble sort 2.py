3cf4f20866f0d1cbb0dfcc7330c085c6f279bd31
Sam Berry <bsam.1479x@gmail.com>
2025-11-24 10:23:38 +0000

    bubble sort 2
    Added comments to explain the bubble sort algorithm.

--- bubble sort 2.py
+++ bubble sort 2.py
@@ -1,17 +1,18 @@
 items = [23, 42, 11, 17, 34, 56]
-n = len(items)-1
-swapped = True
+n = len(items)-1 # n is the number of indexes the list has
+swapped = True # begins a bubble swap
 
-while(swapped and n>0):
-    swapped = False
-    for index in range(0, n):
-        if(items[index] > items[index+1]):
-            temp = items[index]
-            items[index] = items[index+1]
-            items[index+1] = temp
+while(swapped and n>0): # defines whar happens when items arw swapped
+    swapped = False # before any items have been swapped, 'swapped' must be false
+    for index in range(0, n): # for each item in the list:
+        if(items[index] > items[index+1]): # if a swap is possible, meaning it is bigger than the next item in the list:
+            temp = items[index] # placeholder storing the index's data
+            items[index] = items[index+1] # assign the value of the next item to the currrent item
+            items[index+1] = temp # assign the current item's value to the next item
 
-            swapped = True
+            swapped = True # the items are now swapped
 
-    n = n-1
+    n = n-1 # one less comparison is needed
 
-print(items)
+print(items) # print the new list now that it has been swapped
+
